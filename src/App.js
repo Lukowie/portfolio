477e510f2ff41f4489f5619714815fc95d0c1f53
Lukowie <lukevdm64@gmail.com>
2021-07-06 22:55:35 -0400

    added tsparticles to landing page, changed landing page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from './components/About';
 import Work from './components/Work'; 
 import mobileLanding from './img/mobileLanding.jpg';
 import { Typography } from '@material-ui/core';
+import Particles from 'react-particles-js';
 
 //TODO: make buttons the same width, resize with same scale
 //possible add particle effect
@@ -24,23 +25,31 @@ function App() {
           </Route>
           <Route path="/">
 
+          
           <div className="App">
-            <header className="App-header" style={{backgroundSize: "100vw, auto", backgroundImage: `url(${imgUrl})`}}>
-              <p style={{fontSize: "2.5em", maxWidth: "90vw"}}>
+         
+            <Particles className="App-header"/>
+            
+              <span style={{zIndex: "4", top: "15%", left:"50%", transform: "translateX(-50%)", position: "absolute", minWidth: "325px", minHeight: "325px", maxWidth: "800px", maxHeight: "800px", width:"30vw", height:"30vw", backgroundColor: "rgba(0, 0, 0, 0.66)", borderRadius:"50%", border: "2px solid white"}}>
+              <p style={{color: "white", position: "relative", top: "35%", left: "50%", transform: "translate(-50%, -50%)", fontSize: "calc(26px + 2.3vw)"}}>
                 Lucas Vandermaarel
               </p>
-              <div style={{display: "inline-block"}}>
-                <Button variant="contained" id="workBtn"><Link style={{textDecoration: "none", color: "#ccc"}} to="/work">View Work</Link></Button>
-                <Button variant="contained" id="aboutBtn"><Link style={{textDecoration: "none", color: "#ccc"}} to="/about">About Me</Link></Button>
-              </div>
-
-            </header>
+              </span>
             </div>
+           
           </Route>
         </Switch>
     </Router>
     
   );
+
+  /* 
+  <div style={{display: "inline-block"}}>
+      <Button variant="contained" id="workBtn"><Link style={{textDecoration: "none", color: "#ccc"}} to="/work">View Work</Link></Button>
+      <Button variant="contained" id="aboutBtn"><Link style={{textDecoration: "none", color: "#ccc"}} to="/about">About Me</Link></Button>
+  </div>
+
+              */
 }
 
 export default App;
