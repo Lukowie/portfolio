0a4e33a6c220b51993ac422ef92bad826cedd273
Lukowie <lukevdm64@gmail.com>
2021-07-08 23:37:22 -0400

    Fixed bugs with resizing issues on btn. Made components to make code look cleaner

--- src/App.js
+++ src/App.js
@@ -1,55 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
-import {Button} from '@material-ui/core';
-import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import About from './components/About';
 import Work from './components/Work'; 
-import mobileLanding from './img/mobileLanding.jpg';
-import { Typography } from '@material-ui/core';
+import Home from './components/Home';
 import Particles from 'react-particles-js';
+import LandingBtn from './components/landingBtn';
 
 //TODO: make buttons the same width, resize with same scale
-//possible add particle effect
-//drawer navigation links 
+//possible add particle effect -- done
+//drawer navigation links  -- removed
 
 function App() {
-  const imgUrl = window.innerWidth >=650 ? mobileLanding : mobileLanding;
   return (
     <Router>
         <Switch>
           <Route path="/work">
             <Work />
           </Route>
+          <Route path="/home">
+            <Home/>
+          </Route>
           <Route path="/about">
             <About />
           </Route>
           <Route path="/">
 
-          
           <div className="App">
          
             <Particles className="App-header"/>
+              <LandingBtn/>
             
-              <span style={{zIndex: "4", top: "15%", left:"50%", transform: "translateX(-50%)", position: "absolute", minWidth: "325px", minHeight: "325px", maxWidth: "800px", maxHeight: "800px", width:"30vw", height:"30vw", backgroundColor: "rgba(0, 0, 0, 0.66)", borderRadius:"50%", border: "2px solid white"}}>
-              <p style={{color: "white", position: "relative", top: "35%", left: "50%", transform: "translate(-50%, -50%)", fontSize: "calc(26px + 2.3vw)"}}>
-                Lucas Vandermaarel
-              </p>
-              </span>
             </div>
+          
            
           </Route>
         </Switch>
     </Router>
-    
   );
 
-  /* 
-  <div style={{display: "inline-block"}}>
-      <Button variant="contained" id="workBtn"><Link style={{textDecoration: "none", color: "#ccc"}} to="/work">View Work</Link></Button>
-      <Button variant="contained" id="aboutBtn"><Link style={{textDecoration: "none", color: "#ccc"}} to="/about">About Me</Link></Button>
-  </div>
-
-              */
 }
 
 export default App;
---
+++ src/components/landingBtn.js
@@ -0,0 +1,66 @@
+import React from "react";
+import styled from "styled-components";
+
+function handleClick(){
+    // animation to blow up screen with color
+    console.log("yup");
+
+    window.location.href = "/home";
+  }
+
+
+  const Wrapper = styled.div`
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const Btn = styled.span`
+  z-index: 4;
+  position: relative;
+  min-width: 200px;
+  min-height: 200px;
+  max-width: 600px;
+  max-height: 600px;
+  width:25vw;
+  height:25vw;
+  background-color: rgba(0, 0, 0, 0.2);
+  border-radius: 50%;
+  border: 0.2vw solid white;
+  transition: all 1.5s ease-in-out;
+  :hover{
+      transform: scale(0.8);
+  } 
+`;
+
+const Text = styled.p`
+  color: white; 
+  position: relative;
+  top: 35%; 
+  left: 50%; 
+  transform: translate(-50%, -50%); 
+  font-size: ${props => props.fontSize};
+  // make text unselectable
+  -webkit-user-select: none; /* Safari */        
+  -moz-user-select: none; /* Firefox */
+  -ms-user-select: none; /* IE10+/Edge */
+  user-select: none; /* Standard */
+`;
+
+function LandingBtn(){
+    return(
+        <Wrapper>
+            <Btn onClick={handleClick}>
+                <Text fontSize="calc(2vw + 2vh)" >
+                    Lucas Vandermaarel
+                </Text>
+            </Btn>
+        </Wrapper>
+    );
+}
+
+
+export default LandingBtn;
